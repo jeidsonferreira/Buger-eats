b9d075e8e26079e5e51bbc634c11cf3f0d84fda8
Jeidson Ferreira <jeidsonferreira89@gmail.com>
2022-12-20 23:20:36 -0300

    adicionando informações do dashboard do Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {   
+  e2e: {
+      projectId: "u2zw8q",
       viewportWidth: 1440,
       viewportHeight: 900,
       baseUrl: 'https://buger-eats-qa.vercel.app'
