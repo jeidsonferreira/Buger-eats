7a713a31790f71e32806a03ef8a25cb449defa4d
Jeidson Ferreira <jeidsonferreira89@gmail.com>
2022-12-23 15:39:29 -0300

    Atualizando arquivo config e temp

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "u2zw8q",
   e2e: {
-      projectId: "u2zw8q",
       viewportWidth: 1440,
       viewportHeight: 900,
       baseUrl: 'https://buger-eats-qa.vercel.app'
        
     //..setupNodeEvents(on, config) {
-      // implement node event listeners here      
-    
-       
+      // implement node event listeners here       
     //},
   },
 
